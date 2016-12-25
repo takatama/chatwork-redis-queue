107450992bc6dce822cde89fd03fdb00396a9f3a
takatama <hirokazu.takatama@gmail.com>
2016-12-25 12:07:32 +0900

    extracts pushBack and suspendWorker.

--- src/worker.js
+++ src/worker.js
@@ -5,6 +5,29 @@ var fs = require('fs');
 var request = require('request');
 var token;
 
+var pushBack = function (job) {
+    return messages.add(job.data, {
+        lifo: true,
+        removeOnComplete: true
+    });
+};
+
+var suspendWorker = function (millisec) {
+    return new Promise(function (resolve, reject) {
+        messages.pause().then(function () {
+            setTimeout(function () {
+                messages.resume().then(function () {
+                    resolve();
+                }).catch(function (err) {
+                    reject(err);
+                });
+            }, millisec);
+        }).catch(function (err) {
+            reject(err);
+        });
+    });
+};
+
 var sendMessage = function (job, done) {
     var roomId = job.data.roomId;
     var message = job.data.message;
@@ -24,17 +47,11 @@ var sendMessage = function (job, done) {
             var resetTime = parseInt(response.headers['x-ratelimit-reset'] + '000', 10);
             var waitMillisec = resetTime - Date.now();
             console.log('Rate limit will be reset at ' + new Date(resetTime) + ' (after ' + waitMillisec + ' millisec).');
-            messages.add(job.data, {
-                lifo: true,
-                removeOnComplete: true
-            });
             done(new Error('Rate limit'));
-            messages.pause().then(function () {
-               setTimeout(function () {
-                   messages.resume().then(function () {
-                       console.log('...resumed at ' + new Date());
-                   });
-               }, waitMillisec);
+            pushBack(job).then(function () {
+                suspendWorker(waitMillisec).then(function () {
+                    console.log ('...resumed at ' + new Date());
+                });
             });
         } else if (response.statusCode === 200) {
             var res = JSON.parse(body);
